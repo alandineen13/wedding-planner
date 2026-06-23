c5442d93479f8bc62d2cd837e97774079d078bee
AlanDineen <adineenbusiness@gmail.com>
2026-06-23 22:44:02 +0100

    updating apiURL

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://api-wedding-planner-cjh9avbdb0hsd4gf.westeurope-01.azurewebsites.net/api',
 };
